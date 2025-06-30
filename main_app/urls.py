b260510eb151a1699de14c17f3c951e11e3055a2
egorovaelizaveta041@gmail.com <egorovaelizaveta041@gmail.com>
2025-06-30 16:53:49 +0300

    laba 15

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import about_author, main_page, about_stationery_store
+from .views import about_author, main_page, about_stationery_store, speciality, speciality_search
 
 urlpatterns = [
     path('', main_page, name='main_page'),
     path('about_author', about_author, name='about_author'),
-    path('about_stationery_store', about_stationery_store, name='about_stationery_store')
+    path('about_stationery_store', about_stationery_store, name='about_stationery_store'),
+    path('spec/', speciality, name='speciality'),
+    path('speciality_search', speciality_search, name='speciality_search')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,3 +10,37 @@ def about_author(request):
 
 def about_stationery_store(request):
     return render(request, 'about_stationery_store.html')
+
+def speciality(request):
+    specialties = []  
+    with open("dump.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        
+    for specialty in data:
+        if specialty.get("model") == "data.specialty":
+            specialty_data = {
+                "code": specialty["fields"].get("code"),
+                "pk": specialty.get("pk"),
+                "title": specialty["fields"].get("title"),
+                "c_type": specialty["fields"].get("c_type"),
+            }
+            specialties.append(specialty_data)  
+    return render(request, 'speciality.html',{'specialties': specialties})
+
+def speciality_search(request):
+    id = request.GET.get('code') 
+    speciality_list = []
+    with open("dump.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+        for speciality in data:
+            if speciality.get('model') == "data.specialty" and (speciality["fields"].get("code") == id):
+                    speciality_data = {
+                        "code": speciality["fields"].get("code"),
+                        "pk": speciality["pk"],
+                        "title": speciality["fields"].get("title"),
+                        "c_type": speciality["fields"].get("c_type"),
+                    }
+                    speciality_list.append(speciality_data)
+
+    return render(request, "speciality_search.html", {'speciality': speciality_list})
